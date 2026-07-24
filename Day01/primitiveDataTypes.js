e7d683eaf88709f6a0f5739cecad6a4d60b28676
logeshwariragu1202-del <logeshwariragu1202@gmail.com>
2026-07-24 22:47:02 +0530

    Add files via upload
    Day01 Assignment

---
+++ Day01/primitiveDataTypes.js
@@ -0,0 +1,15 @@
+let firstName='Logeshwari'
+   console.log(firstName)
+let companyName
+   companyName='Amazon'  
+   console.log(companyName)
+let mobileNumber=7358091578
+   console.log(mobileNumber)
+let  isAutomation=false 
+   console.log(isAutomation)
+let  hasPlaywright
+   console.log(hasPlaywright)
+
+//Print and confirm the values and data types
+console.log(typeof firstName,typeof companyName,typeof mobileNumber,typeof isAutomation,typeof hasPlaywright)
+
