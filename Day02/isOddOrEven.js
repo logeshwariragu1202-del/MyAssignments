fbcd11ad7b94cdcddc1ceb0d2f28f7aed0eca379
logeshwariragu1202-del <logeshwariragu1202@gmail.com>
2026-07-24 22:49:08 +0530

    Add files via upload

---
+++ Day02/isOddOrEven.js
@@ -0,0 +1,12 @@
+let num=6
+function numValue(){
+    if(num%2===0){
+        console.log("the given number "+num+ " is even number")
+
+    }else{
+     console.log("the given number "+num+ " is odd number")
+    }
+
+       
+}
+numValue()
